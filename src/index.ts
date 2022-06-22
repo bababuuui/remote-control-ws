e1f243cd0e683706118b96efb15895f970cdefe6
bababuuui <bubinatar@gmail.com>
2022-06-22 22:17:37 +0200

    refactoring - remove comment

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,6 @@ import { wsConnectionHandler } from "./websocket_server/handlers";
 dotenv.config();
 const HTTP_PORT = process.env.PORT || 3000;
 const WS_PORT = parseInt(process.env.WS_PORT) || 8080;
-// for testing purposes. Waits ${ms} before drawing object.
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
