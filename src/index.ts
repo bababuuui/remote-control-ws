b2f184be2cb4c05286148e0629be11729f44f63c
bababuuui <bubinatar@gmail.com>
2022-06-22 15:10:54 +0200

    refactor - use streams api

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
-import { WebSocketServer } from "ws";
+import { createWebSocketStream, WebSocketServer } from "ws";
 import robot from "robotjs";
+import { Duplex } from "stream";
 import { httpServer } from "./http_server";
 import { getCurrentMousePosition } from "./commands/commonCommands";
 import { drawCircle, drawRectangle } from "./commands/drawCommands";
@@ -10,6 +11,8 @@ import { takeScreenshot } from "./utils/screenshotUtils";
 dotenv.config();
 const HTTP_PORT = process.env.PORT || 3000;
 const WS_PORT = process.env.WS_PORT || 3000;
+// for testing purposes
+const WAIT_BEFORE_DRAW_MS = 1000;
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
 
@@ -22,10 +25,13 @@ httpServer.listen(HTTP_PORT);
 const wss = new WebSocketServer({ port: WS_PORT });
 
 wss.on("connection", async function connection(ws) {
+  console.log("Connection started");
+  const wsStream: Duplex = createWebSocketStream(ws, { decodeStrings: false });
   ws.on("message", async function message(data) {
+    console.log("received: %s", data);
     const [command, ...args] = data.toString().split(" ");
     const { x, y } = getCurrentMousePosition();
-    console.log(x, y);
+    let webSocketResponse = command;
     switch (command) {
       case "mouse_up":
         robot.moveMouse(x, y - parseInt(args[0]));
@@ -40,32 +46,34 @@ wss.on("connection", async function connection(ws) {
         robot.moveMouse(x + parseInt(args[0]), y);
         break;
       case "mouse_position":
-        console.log(x, y);
-        ws.send(`mouse_position ${x},${y}`);
+        webSocketResponse = `mouse_position ${x},${y}`;
         break;
       case "draw_circle":
-        await waitForMs(1000);
+        await waitForMs(WAIT_BEFORE_DRAW_MS);
         drawCircle(parseInt(args[0]));
         break;
       case "draw_rectangle":
-        await waitForMs(1000);
+        await waitForMs(WAIT_BEFORE_DRAW_MS);
         const width = parseInt(args[0]);
         const length = parseInt(args[1]);
         drawRectangle(width, length);
         break;
       case "draw_square":
-        await waitForMs(1000);
+        await waitForMs(WAIT_BEFORE_DRAW_MS);
         drawRectangle(parseInt(args[0]), parseInt(args[0]));
         break;
 
       case "prnt_scrn":
-        const base64Screenshot = takeScreenshot(x, y, 100, 100);
-        ws.send(`prnt_scrn ${base64Screenshot}`);
+        const base64Screenshot = takeScreenshot(x, y, 200, 200);
+        webSocketResponse = `prnt_scrn ${base64Screenshot}`;
         break;
       default:
-      // ws.send("default");
     }
-    console.log("received: %s", data);
+    wsStream.write(webSocketResponse);
+  });
+
+  ws.on("close", () => {
+    console.log("Connection closed");
   });
 
   process.on("SIGINT", () => {
